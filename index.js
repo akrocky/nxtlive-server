bb7ffef3b3108b2a7043de776083f72ed2b29f0e
akrocky <abulkashemrocky@gmail.com>
2023-09-22 00:58:16 +0200

    update index

--- index.js
+++ index.js
@@ -1,38 +1,78 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
-const cors = require('cors');
-const socketServer=require('./socketServer');
+
+
 const app = express();
-const server   = require('http').Server(app);
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+const server = http.createServer(app);
 
-// Enable CORS for all routes
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  
- 
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+const io = socketIo(server, {
+  cors:{
+    origin:'*',
+    methods:['GET','POST']
+}
+ });
 
 
-
-// Enable CORS for all routes (adjust the options as needed)
 
 app.get('/test', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
-socketServer.registerSocketServer(server)
 
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+console.log(__filename)
 
+io.sockets.on('error', e => console.log(e));
+var broadcaster;
+io.on('connection', function (socket) {
+  console.log("socket connection",socket.id);
+   socket.on('broadcaster', function () {
+      //id of the broadcaster
+      broadcaster = socket.id;
+      console.log("send broadcaster"+broadcaster)
+      socket.broadcast.emit('broadcaster');
+   });
+   //Default room
+   // Each Socket in Socket.IO is identified by a random, unguessable, unique identifier Socket#id. 
+   //For your convenience, each socket automatically joins a room identified by this id.
+   socket.on('watcher', function () {
+      //tell to broadcast there is a watcher
+     console.log("conected",socket.id);
+	  if(!(broadcaster=== undefined))
+	  {
+     
+         broadcaster && socket.to(broadcaster).emit('watcher', socket.id);
+		  console.log("send watcher");
+      }
+   });
 
+   //send sdp to the client
+   socket.on('offer', function (id /* of the watcher */, message) {
+      console.log("send offer")
+      socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
+   });
+   //send sdp of the client to broad caster
+   socket.on('answer', function (id /* of the broadcaster */, message) {
+      console.log("send answer")
+      socket.to(id).emit('answer', socket.id /* of the watcher */, message);
+   });
+
+   //exchange ice candidate
+   socket.on('candidate', function (id, message) {
+      console.log("candidate")
+      socket.to(id).emit('candidate', socket.id, message);
+   });
+
+   socket.on('disconnect', function () {
+      console.log("disconnect")
+      broadcaster && socket.to(broadcaster).emit('bye', socket.id);
+   });
+
+});
 
 var port = process.env.PORT || 8000;
 server.listen(port, function () {
