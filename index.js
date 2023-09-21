e0890a5f741b17b110953c475b984d30003586a1
akrocky <abulkashemrocky@gmail.com>
2023-09-21 11:19:31 +0200

    origin alloet

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const server   = require('http').Server(app);
 app.use(express.json())
 app.use(cors())
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  
+ 
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+
 
 // Enable CORS for all routes (adjust the options as needed)
 
